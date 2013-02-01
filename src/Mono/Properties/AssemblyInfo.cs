259946321e7ed3fd402f2e5ed0b73caccaa163e5
Deyan Varchev <deyan.varchev@gmail.com>
2013-02-01 17:28:16 +0200

    Added TaskEx to act as shim for Mono

---
+++ src/Mono/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyProduct("AsyncBridge")]
+[assembly: AssemblyTitle("AsyncBridge")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: ComVisible(false)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: Guid("99fe7ed6-5658-448d-97c5-7fb9b342803d")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
---
+++ src/Mono/Threading.Tasks/TaskEx.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace System.Threading.Tasks
+{
+	public static class TaskEx
+	{
+		public static Task Run(Action action)
+		{
+			return Task.Run(action);
+		}
+
+		public static Task Run(Action action, CancellationToken cancellationToken)
+		{
+			return Task.Run(action, cancellationToken);
+		}
+
+		public static Task<TResult> Run<TResult>(Func<TResult> function)
+		{
+			return Task.Run<TResult>(function);
+		}
+
+		public static Task<TResult> Run<TResult>(Func<TResult> function, CancellationToken cancellationToken)
+		{
+			return Task.Run<TResult>(function, cancellationToken);
+		}
+
+		public static Task Run(Func<Task> function)
+		{
+			return Task.Run(function);
+		}
+
+		public static Task Run(Func<Task> function, CancellationToken cancellationToken)
+		{
+			return Task.Run(function, cancellationToken);
+		}
+
+		public static Task<TResult> Run<TResult>(Func<Task<TResult>> function)
+		{
+			return Task.Run<TResult>(function);
+		}
+
+		public static Task<TResult> Run<TResult>(Func<Task<TResult>> function, CancellationToken cancellationToken)
+		{
+			return Task.Run<TResult>(function, cancellationToken);
+		}
+
+
+        public static Task WhenAll(params Task[] tasks)
+        {
+            return TaskEx.WhenAll((IEnumerable<Task>)tasks);
+        }
+
+        public static Task<TResult[]> WhenAll<TResult>(params Task<TResult>[] tasks)
+        {
+            return TaskEx.WhenAll<TResult>((IEnumerable<Task<TResult>>)tasks);
+        }
+
+      
+        public static Task WhenAll(IEnumerable<Task> tasks)
+        {
+            return Task.WhenAll(tasks);
+        }
+
+        public static Task<TResult[]> WhenAll<TResult>(IEnumerable<Task<TResult>> tasks)
+        {
+            return Task.WhenAll<TResult>(tasks);
+        }
+   
+        public static Task<Task> WhenAny(params Task[] tasks)
+        {
+            return TaskEx.WhenAny((IEnumerable<Task>)tasks);
+        }
+
+        public static Task<Task> WhenAny(IEnumerable<Task> tasks)
+        {
+            return Task.WhenAny(tasks);
+        }
+
+        public static Task<Task<TResult>> WhenAny<TResult>(params Task<TResult>[] tasks)
+        {
+            return TaskEx.WhenAny<TResult>((IEnumerable<Task<TResult>>)tasks);
+        }
+
+        public static Task<Task<TResult>> WhenAny<TResult>(IEnumerable<Task<TResult>> tasks)
+        {
+            return Task.WhenAny<TResult>(tasks);
+        }
+
+	}
+}
